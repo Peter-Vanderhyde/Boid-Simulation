e645b607d48503f70c76a40120e8daad7b7dc9a2
Peter-Vanderhyde <peter.vanderhyde@gmail.com>
2023-02-13 01:52:53 -0600

    Created a basic window and created a basic Boid class with no functionality

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import tkinter
+import threading
+import random
+
+
+if __name__ == "__main__":
+    window = tkinter.Tk()
+
+    canvas = tkinter.Canvas(window, background="white", width=1100, height=600)
+    canvas.pack()
+
+    window.mainloop()
